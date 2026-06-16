4b93359cc8c40d1d42ae3df513981baf9edb873c
Maur025 <mmoyapalli@gmail.com>
2026-06-16 01:19:44 -0400

    feat: add script to update pods

---
+++ update-pods.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+shopt -s expand_aliases
+alias kubectl="minikube kubectl --"
+
+echo "Deploying application to production environment..."
+
+kubectl rollout restart deployment inventory-deployment
+kubectl rollout restart deployment payment-deployment
+kubectl rollout restart deployment people-deployment
+kubectl rollout restart deployment purchase-deployment
+kubectl rollout restart deployment sale-deployment
+kubectl rollout restart deployment gateway-deployment
+
+kubectl rollout status deployment gateway-deployment
+
+echo "All pods have been updated successfully."
